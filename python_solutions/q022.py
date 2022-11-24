49996ab24937204bce4d7e8b61809e289ae41b36
hiroto0222 <aoyamahiroto0222@gmail.com>
2022-11-24 17:12:00 +0900

    q 22

---
+++ python_solutions/q022.py
@@ -0,0 +1,28 @@
+# Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over five-thousand first names, begin by sorting it into alphabetical order.
+# Then working out the alphabetical value for each name, multiply this value by its alphabetical position in the list to obtain a name score.
+
+# For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list.
+# So, COLIN would obtain a score of 938 × 53 = 49714.
+
+# What is the total of all the name scores in the file?
+
+NAMES = open("..\miscellaneous\p022_names.txt").read().replace('"', '').split(",")
+
+
+def calculateScore(i, name):
+    return (i + 1) * sum(
+        ord(c) - 64
+        for c in name
+    )
+
+
+def compute():
+    NAMES.sort()
+    ans = 0
+    for i, name in enumerate(NAMES):
+        ans += calculateScore(i, name)
+    return ans
+
+
+if __name__ == "__main__":
+    print(compute())
